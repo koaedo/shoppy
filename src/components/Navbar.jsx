e949469b7f970e13ff281206b75a7594171c7a50
koaedo_com_pc <koaedo@gmail.com>
2023-01-09 17:30:52 +0900

    firebase를 이용한 로그인/로그아웃 기능 구현

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
+import { login , logout, onUserStateChange } from "../api/firebase";
+import User from "./User";
 
 export default function Navbar() {
+    const [user, setUser] = useState();
+
+    useEffect(()=>{
+        onUserStateChange(setUser);
+    },[])
+
     return (
         <header className="flex justify-between border-b border-gray-300">
             <Link to='/' className="flex items-center text-4xl text-brand">
@@ -14,7 +22,9 @@ export default function Navbar() {
                 <Link to='/products'>Products</Link>
                 <Link to='/carts'>Carts</Link>
                 <Link to='/products/new' className="text-2xl"><BsFillPencilFill /></Link>
-                <button>Login</button>
+                {user && <User user={user} />}
+                {!user && <button onClick={login}>Login</button>}
+                {user && <button onClick={logout}>Logout</button>}
             </nav>
         </header>
     );
